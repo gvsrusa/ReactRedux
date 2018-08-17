1225432a2592d1ad215f16ae23feb77814dd974e
gvsrusa <gvsrusa@gmail.com>
2018-08-17 10:29:37 -0600

    rector both user-card and hello-redux app

--- hello-redux/src/App.js
+++ hello-redux/src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {createStore} from 'redux';
+
+import HelloWorld from './HelloWorld';
+import reducer from './reducers/Reducer';
+
+
+const initialState = { tech: "React" };
+const store = createStore(reducer, initialState);
 
 class App extends Component {
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <HelloWorld tech = {store.getState().tech} />    
   }
 }
 
