352a3d676a62cbcbf3cb54c84dcab949a7c92a64
gvsrusa <gvsrusa@gmail.com>
2018-09-05 23:43:09 -0400

    Commit

---
+++ contact-manager/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Subbu GV</h4>
+                <ul>
+                    <li>Email:</li>
+                    <li>Phone::</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Contact;
