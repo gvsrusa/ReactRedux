4632e032a3a184b9905c7d9927588d0a96636350
gvsrusa <gvsrusa@gmail.com>
2018-10-31 14:21:12 -0700

    Redux

---
+++ redux-book-search/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './containers/app';
+import store from './store';
+import { fetchBooks, setTopic } from './actions';
+
+store.dispatch(setTopic('border collies'));
+store.dispatch(fetchBooks());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
